25b2cfc1b2a872d0f505e25a7b72f6dbb1b59359
mingfun wong <mingfun.wong.chn@gmail.com>
2019-03-08 18:13:04 +0900

    add json2jsonp white list

--- api/index.js
+++ api/index.js
@@ -19,7 +19,7 @@ console.log("v1.0.1");
 app.use(compression());
 
 //allow custom header and CORS
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
@@ -28,24 +28,24 @@ app.use(function(req, res, next) {
 // 首页
 app.get('/', function (req, res) {
   res.send(
-	  	'<a href="/bp_status">/bp_status</a><br>'+
-	  	'<a href="/bp_status_change_logs">/bp_status_change_logs</a><br>'+
-	  	'<a href="/check_p2p?host=p2p-mainnet.fibos123.com&port=9977">/check_p2p</a><br>'+
-	  	// '<a href="/bp_info?bpname=fibos123comm">/bp_info</a><br>'+
-	  	// '<a href="/bp_history?bpname=fibos123comm">/bp_history</a><br>'+
-	  	'<a href="/json2jsonp?url='+encodeURIComponent("https://fibos.io/getExchangeInfo")+'&callback=call">/json2jsonp</a><br>'+
-	  	'<center><img src="https://i.pinimg.com/originals/40/f4/82/40f4820842b40cca27a935d7906af3c9.jpg" width="400" /><br/>Gakki</center>'
-  	);
+    '<a href="/bp_status">/bp_status</a><br>' +
+    '<a href="/bp_status_change_logs">/bp_status_change_logs</a><br>' +
+    '<a href="/check_p2p?host=p2p-mainnet.fibos123.com&port=9977">/check_p2p</a><br>' +
+    // '<a href="/bp_info?bpname=fibos123comm">/bp_info</a><br>'+
+    // '<a href="/bp_history?bpname=fibos123comm">/bp_history</a><br>'+
+    '<a href="/json2jsonp?url=' + encodeURIComponent("https://fibos.io/getExchangeInfo") + '&callback=call">/json2jsonp</a><br>' +
+    '<center><img src="https://i.pinimg.com/originals/40/f4/82/40f4820842b40cca27a935d7906af3c9.jpg" width="400" /><br/>Gakki</center>'
+  );
 });
 
 // BP 在线状态
 app.get('/bp_status', function (req, res) {
-    res.json(bp_status.bp_status());
+  res.json(bp_status.bp_status());
 });
 
 // BP 在线状态变更记录
 app.get('/bp_status_change_logs', function (req, res) {
-    res.json(bp_status.bp_status_change_logs());
+  res.json(bp_status.bp_status_change_logs());
 });
 
 // BP 的出块统计、最终出块时间
@@ -62,15 +62,15 @@ app.get('/bp_status_change_logs', function (req, res) {
 
 // P2P 状态检查
 app.get('/check_p2p', function (req, res) {
-	var host = req.query.host;
-	if (!/^[0-9a-z\.\-]*$/ig.test(host)) {
-		res.send({});
-		return;
-	}
-	var port = parseInt(req.query.port);
-	check_p2p.check_p2p(host, port, function(data){
-		res.json(data)
-	})
+  var host = req.query.host;
+  if (!/^[0-9a-z\.\-]*$/ig.test(host)) {
+    res.send({});
+    return;
+  }
+  var port = parseInt(req.query.port);
+  check_p2p.check_p2p(host, port, function (data) {
+    res.json(data)
+  })
 });
 
 // 出块历史
@@ -88,15 +88,20 @@ app.get('/check_p2p', function (req, res) {
 
 // json2jsonp
 app.get('/json2jsonp', function (req, res) {
-	var url = req.query.url;
-	if (url != "https://fibos.io/getExchangeInfo") {
-		res.send({});
-		return;
-	}
-    var callback = req.query.callback;
-    json2jsonp.json2jsonp(url, callback, function(data){
-    	res.send(data);
-    })
+  var url = req.query.url;
+  const whiteList = [
+    'https://fibos.io/getExchangeInfo',
+    'https://api.bit.cc/ticker.php?c=fo&mk_type=btc',
+    'https://api.bit.cc/ticker.php?c=eos&mk_type=btc',
+  ]
+  if (whiteList.indexOf(url) === 0) {
+    res.send({});
+    return;
+  }
+  var callback = req.query.callback;
+  json2jsonp.json2jsonp(url, callback, function (data) {
+    res.send(data);
+  })
 });
 
 
