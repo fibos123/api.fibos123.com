083dabc973f7940ac047c19f6a20cd49d8a20bf6
mingfun wong <mingfun.wong.chn@gmail.com>
2019-03-08 18:26:24 +0900

    json2jsonp call to Optional

--- api/lib/json2jsonp.js
+++ api/lib/json2jsonp.js
@@ -1,10 +1,13 @@
 const request = require('request');
 
-function json2jsonp(url, call, callback){
-	request.get(url, {timeout: 1000}, function (err, response, body) {
-		var json = (err) ? JSON.stringify(err) : body;
-		callback(call + "(" + body + ")");
-	})
+function json2jsonp(url, call, callback) {
+  request.get(url, { timeout: 1000 }, function (err, response, body) {
+    callback(
+      call
+        ? call + "(" + body + ")"
+        : body
+    )
+  })
 };
 
 exports.json2jsonp = json2jsonp;
