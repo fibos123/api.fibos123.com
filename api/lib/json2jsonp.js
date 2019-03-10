09f08e7bf7f77b5fb3c63cc263caddd4d897aad2
mingfun wong <mingfun.wong.chn@gmail.com>
2019-03-10 18:48:05 +0900

    add json2jsonp temp

--- api/lib/json2jsonp.js
+++ api/lib/json2jsonp.js
@@ -1,12 +1,37 @@
 const request = require('request');
 
+var json2jsonp_temp = {};
+
 function json2jsonp(url, call, callback) {
-  request.get(url, { timeout: 1000 }, function (err, response, body) {
+
+  if (
+    json2jsonp_temp[url]
+    && json2jsonp_temp[url].time + 20 * 1000 > new Date().getTime()
+  ) {
+    console.log("json2jsonp temp", url)
+    const body = json2jsonp_temp[url].payload
     callback(
       call
         ? call + "(" + body + ")"
         : body
     )
+    return
+  }
+
+  request.get(url, { timeout: 1000 }, function (err, response, body) {
+
+    console.log("json2jsonp get", url)
+    json2jsonp_temp[url] = {
+      time: new Date().getTime(),
+      payload: body
+    }
+
+    callback(
+      call
+        ? call + "(" + body + ")"
+        : body
+    )
+
   })
 };
 
