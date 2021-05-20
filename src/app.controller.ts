b7b4d2b127349cf3a25d7940d57dcbbced625e5b
Mingfun Wong <mingfun.wong.chn@gmail.com>
2021-05-20 09:14:50 +0900

    Update app.controller.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,7 +9,7 @@ export class AppController {
 <a href="/bp_status">/bp_status</a><br>
 <a href="/last_ghost">/last_ghost</a><br>
 <a href="/last_snapshot">/last_snapshot</a><br>
-<center><img src="https://i.pinimg.com/originals/40/f4/82/40f4820842b40cca27a935d7906af3c9.jpg" width="400" /><br/>Gakki</center>
+<center><img src="https://pbs.twimg.com/media/DbxJHDDV0AI8bOt?format=jpg&name=4096x4096" width="400" /><br/>Lillie (Pokémon)</center>
 `
   }
 
