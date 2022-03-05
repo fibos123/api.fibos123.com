34150f8a3482c3d9a10fe29320a0e7712b1b6040
Mingfun Wong <mingfun.wong.chn@gmail.com>
2022-03-06 00:37:41 +0900

    update port

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  const port = 8080;
+  const port = process.env.PORT || 8080;
   console.log('App listening on port', port);
   await app.listen(port);
 }
