262cc97e807b4be24399084deb8a2864f054033f
Mingfun Wong <mingfun.wong.chn@gmail.com>
2022-05-30 17:41:53 +0900

    Update last-snapshot.controller.ts

--- src/last-snapshot/last-snapshot.controller.ts
+++ src/last-snapshot/last-snapshot.controller.ts
@@ -8,7 +8,7 @@ export class LastSnapshotController {
 
   @Get()
   async getHello(): Promise<string> {
-    const url = "https://fo.blockeden.cn/"
+    const url = "https://fo.blockeden.cn:446/"
     let uri = ""
     const { data } = await axios.get(url);
     uri = $(data).find("a:last").attr("href")
