2f40ddfee13ea04819f5569a9f6ce1737f9aedca
Mingfun Wong <mingfun.wong.chn@gmail.com>
2023-12-16 22:31:14 +0900

    update to nestjs 10.2.10

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,10 +5,10 @@ export class AppController {
   @Get()
   getHello(): string {
     return `
+<html>
 <a href="/bp_status">/bp_status</a><br>
-<a href="/last_ghost">/last_ghost</a><br>
 <a href="/last_snapshot">/last_snapshot</a><br>
 <center><img src="https://pbs.twimg.com/media/DbxJHDDV0AI8bOt?format=jpg&name=4096x4096" width="400" /><br/>Lillie (Pokémon)</center>
-`;
+</html>`;
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
-import { LastGhostController } from './last-ghost/last-ghost.controller';
 import { LastSnapshotController } from './last-snapshot/last-snapshot.controller';
 import { BpStatusModule } from './bp-status/bp-status.module';
 import { VersionModule } from './version/version.module';
 
 @Module({
   imports: [ConfigModule.forRoot(), BpStatusModule, VersionModule],
-  controllers: [AppController, LastGhostController, LastSnapshotController],
+  controllers: [AppController, LastSnapshotController],
   providers: [],
 })
 export class AppModule {}
